6c681a461aa23142f258b665c2d97571a13e5800
vindokum <vinodkumar3693@gmail.com>
2023-02-19 18:19:10 +0530

    change ID

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 const heading1 = React.createElement("h1", { id: "heading" }, "heading 1");
-const heading2 = React.createElement("h1", { id: "heading" }, "heading 2");
+const heading2 = React.createElement("h1", { id: "heading2" }, "heading 2");
 const container = React.createElement("div", {}, [heading1, heading2]);
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(container);
